d562c4907e50acce3fa5c53e02d72e67d1a5c136
zafar-saleem <zafar@Zafars-MacBook-Pro.local>
2014-04-03 21:08:19 +0200

    Added polyfill

--- plugin.js
+++ plugin.js
@@ -1,3 +1,11 @@
+if (typeof Object.create !== 'function') {
+	Object.create = function (obj) {
+		function F() {};
+		F.prototype = obj;
+		return new F();
+	}
+}
+
 ;(function ($) {
 
 	var ModuleName = (function () {
